93704a7fca355c2e6eff881168ca8d2dc62150ef
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:35:33 -0400

    first commit

---
+++ lights/greenlight.h
@@ -0,0 +1,17 @@
+#ifndef GREENLIGHT_H
+#define GREENLIGHT_H
+
+#include <QObject>
+#include "light.h"
+
+class GreenLight : public Light
+{
+    Q_OBJECT
+public:
+    explicit GreenLight(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GREENLIGHT_H
---
+++ lights/yellowlight.h
@@ -0,0 +1,17 @@
+#ifndef YELLOWLIGHT_H
+#define YELLOWLIGHT_H
+
+#include <QObject>
+#include "light.h"
+
+class YellowLight : public Light
+{
+    Q_OBJECT
+public:
+    explicit YellowLight(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // YELLOWLIGHT_H
